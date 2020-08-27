24c7013ea7b04aea5966f70c31940be16fd49ab5
Manasi Palka <mpalkar27@gmail.com>
2020-08-27 03:45:58 +0100

    Third Commit

---
+++ src/com/company/Customer.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Customer {
+
+    String name;
+    int user_id;
+
+    public Customer(String name,int user_id){
+        this.name = name;
+        this.user_id = user_id;
+    }
+}
+
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,14 +14,13 @@ import static java.nio.file.Paths.*;
 
 public final class Main {
 
-    public static void main(String... args) throws IOException {
+    public static void main(String[] args) throws IOException {
         Main parser = new Main("/Users/manasipalkar/Desktop/Assignment/Intercom/Destination/src/CustomerList.txt");
-
         parser.parseLineByLine();
         log("Records parsed");
      //   log(System.getProperty ( "user.dir" ));
     }
-
+      ArrayList<Customer> list = new ArrayList<Customer> ();
     /**
      Constructor
      */
@@ -29,11 +28,14 @@ public final class Main {
         filePath = get(fileName);
     }
 
-    public final void parseLineByLine() throws IOException {
+    public final void parseLineByLine()  {
         try (Scanner scanner =  new Scanner(filePath, ENCODING.name())){
             while (scanner.hasNextLine()){
              parseEachLine(scanner.nextLine());
             }
+        }
+        catch (IOException e){
+            System.out.println (e.getCause ());
         }
     }
 
@@ -49,16 +51,18 @@ public final class Main {
                 String user_id = scanner.next ();
                 String name = scanner.next();
                 String longitude = scanner.next();
-                latitude = latitude.replaceAll("\\p{Ps}", " ");
-                longitude = longitude.replaceAll("}$", " ");
+                latitude = latitude.replaceAll("\\p{Ps}", "");
 
-//               if(getDistance(latitude,longitude, 53.339428, -6.043701)) //returns true print corresponding user_id and user_name)
-//                {
-//               // check for user_id
-//                System.out.println(name.trim() + " "+  "; " + " "+user_id.trim());
-//                }
-             //System.out.println(name.trim() + " "+  "; " + " "+user_id.trim() + " "+  "; " + " "+latitude.trim() + " "+  "; " + " "+longitude.trim());
-               System.out.println(user_id.trim()+ " "+  "; " + " " + name.trim());
+                longitude = longitude.replaceAll("}$", "");
+//
+               if(getDistance(latitude,longitude, 53.339428, -6.043701)) //returns true print corresponding user_id and user_name)
+                {
+               // check for user_id
+                System.out.println(name.trim() + " "+  "; " + " "+user_id.trim());
+                }
+//             //System.out.println(name.trim() + " "+  "; " + " "+user_id.trim() + " "+  "; " + " "+latitude.trim() + " "+  "; " + " "+longitude.trim());
+             //  System.out.println(latitude.trim()+ " "+  "; " + " " + longitude.trim());
+
                 //Store the contents in an  output file
 
             }
@@ -70,20 +74,34 @@ public final class Main {
         }
     }
         // get distance of each cordinate from the office location 53.339428, -6.257664
-    public static final double Radius = 6372.8; // In kilometers;
-    public static boolean getDistance(String latitude, String longitude, double lat2, double long2) {
-        double lat1=Double.parseDouble(latitude);
-        double long1=Double.parseDouble(longitude);
-        double deltaLat = Math.toRadians(lat2 - lat1);
-        double deltaLong = Math.toRadians(long2 - long1);
-        lat1 = Math.toRadians(lat1);
-        lat2 = Math.toRadians(lat2);
+    public static final double Radius; // In kilometers;
 
-        double a = Math.pow(Math.sin(deltaLat / 2),2) + Math.pow(Math.sin(deltaLong / 2),2) * Math.cos(lat1) * Math.cos(lat2);
-        double c = 2 * Math.asin(Math.sqrt(a));
-        double distance =  Radius * c  ;
-      //  System.out.println ("Distance of the customer from the office in km "+ " " + distance);
-        return distance<=100;
+    static {
+        Radius = 6372.8;
+    }
+
+    public static boolean getDistance(String latitude, String longitude, double lat2, double long2) {
+//      System.out.println (double.parseDouble( String.valueOf ( latitude));
+     // double lat1=Double.parseDouble( String.valueOf ( latitude ).getClass () .getName ());
+        try{
+            double lat1 = Double.parseDouble(latitude);
+            System.out.println (lat1);
+        } catch(NumberFormatException e) {
+            System.out.println ("Invalid format");
+        }
+
+   //  double long1=double.parseDouble( String.valueOf ( longitude ) );
+//        double deltaLat = Math.toRadians(lat2 - lat1);
+//        double deltaLong = Math.toRadians(long2 - long1);
+//        lat1 = Math.toRadians(lat1);
+//        lat2 = Math.toRadians(lat2);
+//
+//        double a = Math.pow(Math.sin(deltaLat / 2),2) + Math.pow(Math.sin(deltaLong / 2),2) * Math.cos(lat1) * Math.cos(lat2);
+//        double c = 2 * Math.asin(Math.sqrt(a));
+//        double distance =  Radius * c  ;
+//      System.out.println ("Distance of the customer from the office in km "+ " " + distance);
+//        return distance<=100;
+        return true;
 
     }
     // private objectsgit
