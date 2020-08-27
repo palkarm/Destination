1c7529bc23971c022b90594cee30767cc5346c6e
Manasi Palka <mpalkar27@gmail.com>
2020-08-27 15:06:39 +0100

    Third Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,6 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Objects;
 import java.util.Scanner;
 
@@ -14,13 +12,13 @@ import static java.nio.file.Paths.*;
 
 public final class Main {
 
-    public static void main(String[] args) throws IOException {
-        Main parser = new Main("/Users/manasipalkar/Desktop/Assignment/Intercom/Destination/src/CustomerList.txt");
+    public static void main(String... args) throws IOException {
+        Main parser = new Main("CustomerList.txt");
         parser.parseLineByLine();
-        log("Records parsed");
-     //   log(System.getProperty ( "user.dir" ));
+        log("Done.");
+        //   log(System.getProperty ( "user.dir" ));
     }
-      ArrayList<Customer> list = new ArrayList<Customer> ();
+
     /**
      Constructor
      */
@@ -28,14 +26,11 @@ public final class Main {
         filePath = get(fileName);
     }
 
-    public final void parseLineByLine()  {
+    public final void parseLineByLine() throws IOException {
         try (Scanner scanner =  new Scanner(filePath, ENCODING.name())){
             while (scanner.hasNextLine()){
-             parseEachLine(scanner.nextLine());
+                parseEachLine(scanner.nextLine());
             }
-        }
-        catch (IOException e){
-            System.out.println (e.getCause ());
         }
     }
 
@@ -47,64 +42,45 @@ public final class Main {
             if (scanner.hasNext()) {
 
                 //assumes the line has a certain structure
-              String latitude = scanner.next ();
+                String latitude = scanner.next ();
                 String user_id = scanner.next ();
                 String name = scanner.next();
                 String longitude = scanner.next();
                 latitude = latitude.replaceAll("\\p{Ps}", "");
-
                 longitude = longitude.replaceAll("}$", "");
-//
-               if(getDistance(latitude,longitude, 53.339428, -6.043701)) //returns true print corresponding user_id and user_name)
-                {
-               // check for user_id
-                System.out.println(name.trim() + " "+  "; " + " "+user_id.trim());
-                }
-//             //System.out.println(name.trim() + " "+  "; " + " "+user_id.trim() + " "+  "; " + " "+latitude.trim() + " "+  "; " + " "+longitude.trim());
-             //  System.out.println(latitude.trim()+ " "+  "; " + " " + longitude.trim());
+                if(getDistance(latitude,longitude, 53.339428, -6.257664)<=100.0){
+                    System.out.println(name.trim() + " "+  "; " + " "+user_id.trim());
 
-                //Store the contents in an  output file
+                    //System.out.println(latitude.trim() + " "+  "; " + " "+longitude.trim());
+                }
 
             }
-
             else {
                 log("Empty or invalid line. Unable to process.");
             }
-            scanner.close ();
         }
     }
-        // get distance of each cordinate from the office location 53.339428, -6.257664
-    public static final double Radius; // In kilometers;
+    // get distance of each cordinate from the office location 53.339428, -6.257664
+    public static final double Radius = 6372.8; // In kilometers;
+    public static double getDistance(String latitude, String longitude, double lat2, double long2) {
+        String requiredLatitude = latitude.replaceAll("[a-zA-Z:\"\\s]","");
+        String requiredLongitude = longitude.replaceAll("[a-zA-Z:\"\\s]","");
+        double lat1 = Double.parseDouble(requiredLatitude);
+        double long1 = Double.parseDouble(requiredLongitude);
+        double deltaLat = Math.toRadians(lat2 - lat1);
+        double deltaLong = Math.toRadians(long2 - long1);
+        lat1 = Math.toRadians(lat1);
+        lat2 = Math.toRadians(lat2);
 
-    static {
-        Radius = 6372.8;
+        double a = Math.pow(Math.sin(deltaLat / 2),2) + Math.pow(Math.sin(deltaLong / 2),2) * Math.cos(lat1) * Math.cos(lat2);
+        double c = 2 * Math.asin(Math.sqrt(a));
+        double distance =  Radius * c  ;
+       if(distance<=100)
+       {System.out.println ("Distance " + distance + " km");}
+
+        return distance;
     }
-
-    public static boolean getDistance(String latitude, String longitude, double lat2, double long2) {
-//      System.out.println (double.parseDouble( String.valueOf ( latitude));
-     // double lat1=Double.parseDouble( String.valueOf ( latitude ).getClass () .getName ());
-        try{
-            double lat1 = Double.parseDouble(latitude);
-            System.out.println (lat1);
-        } catch(NumberFormatException e) {
-            System.out.println ("Invalid format");
-        }
-
-   //  double long1=double.parseDouble( String.valueOf ( longitude ) );
-//        double deltaLat = Math.toRadians(lat2 - lat1);
-//        double deltaLong = Math.toRadians(long2 - long1);
-//        lat1 = Math.toRadians(lat1);
-//        lat2 = Math.toRadians(lat2);
-//
-//        double a = Math.pow(Math.sin(deltaLat / 2),2) + Math.pow(Math.sin(deltaLong / 2),2) * Math.cos(lat1) * Math.cos(lat2);
-//        double c = 2 * Math.asin(Math.sqrt(a));
-//        double distance =  Radius * c  ;
-//      System.out.println ("Distance of the customer from the office in km "+ " " + distance);
-//        return distance<=100;
-        return true;
-
-    }
-    // private objectsgit
+    // PRIVATE
     private final Path filePath;
     private final static Charset ENCODING = StandardCharsets.UTF_8;
 
